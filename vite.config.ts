fd9f169f04c30ac38cbad9691ac0718843a5ce30
Hari1307 <harijain57@gmail.com>
2025-01-09 12:38:46 +0530

    urlfix

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'https://shortner-app.onrender.com',  
-        changeOrigin: true,
-        secure:true,
-        // rewrite: (path) => path.replace(/^\/auth/, '/auth'),
-      },
-      '/api': {
-        target: 'https://shortner-app.onrender.com',  
-        changeOrigin: true,
-        secure:true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      }
-    }
-  },
-  build: {
-    outDir:"dist"
-  }
 })
 
